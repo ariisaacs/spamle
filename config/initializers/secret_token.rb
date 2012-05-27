28eebf917d4032a7d1013ea89532a2a2f2141295
ari <ariisaacs@gmail.com>
2012-05-28 02:50:05 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spamle::Application.config.secret_token = '26a2912cc00725b1bea98d023cdc3d7acabc56b8fb224887db065d0efda57b22b40d3b42b115e54ad00b7d7eeca19027fee879e156091883b1541e9e61565da2'
